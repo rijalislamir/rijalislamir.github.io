0d96e87a81ecf9ea7cda02f2917601f07266a72e
rijalislamir <rijalislamir@gmail.com>
2023-02-18 17:41:02 +0700

    change title app

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Rijal" />
+  <HelloWorld msg="Rijal Portfolio" />
 </template>
 
 <style scoped>
