7448ec1ca6179e40207e4144971272fdc86f3e3f
rijalislamir <rijalislamir@gmail.com>
2023-04-16 14:52:27 +0700

    equalize header transition

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,26 @@
 <template>
   <header class="fixed inset-x-8 top-6 text-white overflow-hidden z-50">
-    <div class="flex justify-between">
-      <Transition
-        appear
-        enter-from-class="-translate-y-10"
-        enter-active-class="transition-translate duration-1000"
-        enter-to-class="translate-y-0"
-        leave-from-class="translate-y-0"
-        leave-active-class="transition-translate duration-1000"
-        leave-to-class="-translate-y-10"
-      >
+    <Transition
+      appear
+      enter-from-class="-translate-y-10"
+      enter-active-class="transition-translate duration-1000"
+      enter-to-class="translate-y-0"
+      leave-from-class="translate-y-0"
+      leave-active-class="transition-translate duration-1000"
+      leave-to-class="-translate-y-10"
+    >
+      <div class="flex justify-between">
         <span @click="() => router.push('/')" class="cursor-pointer font-bold">
           RIJAL
         </span>
-      </Transition>
 
-      <div class="flex gap-4">
-        <TransitionGroup
-          tag="div"
-          class="flex gap-4"
-          appear
-          enter-from-class="-translate-y-10"
-          enter-active-class="transition-translate duration-1000"
-          enter-to-class="translate-y-0"
-          leave-from-class="translate-y-0"
-          leave-active-class="transition-translate duration-1000"
-          leave-to-class="-translate-y-10"
-        >
-          <span v-for="(item, i) in rightMenu" v-show="item.show" :key="i">
+        <div class="flex gap-4">
+          <span v-for="(item, i) in rightMenu" :key="i">
             {{ item.text }}
           </span>
-        </TransitionGroup>
+        </div>
       </div>
-    </div>
+    </Transition>
   </header>
 </template>
 
